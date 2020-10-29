e2147a55ffbc49017fc6aceaf81252bdd5e2e8f4
mexjj <15828574775@139.com>
2020-10-30 01:46:42 +0800

    第一次测试

--- src/main/java/com/xjj/springboot1/Springboot1Application.java
+++ src/main/java/com/xjj/springboot1/Springboot1Application.java
@@ -2,7 +2,12 @@ package com.xjj.springboot1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class Springboot1Application {
 
@@ -10,4 +15,8 @@ public class Springboot1Application {
         SpringApplication.run(Springboot1Application.class, args);
     }
 
+    @RequestMapping("/hello/{id}")
+    public @ResponseBody String hello(@PathVariable String id){
+        return "哈哈哈，hheheeh! 123465 === " + id;
+    }
 }
